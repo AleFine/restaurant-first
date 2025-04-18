e89cc3e0b0c13e9f2fef9557d5d63bb1e496e81a
Kevin-2 <keving.kpg@gmail.com>
2025-04-18 10:43:22 -0500

    feat: Add API resource routes for Mesas and Reservas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ComensalController;
+use App\Http\Controllers\Api\MesaController;
+use App\Http\Controllers\Api\ReservaController;
 
 Route::apiResource('comensales', ComensalController::class);
+Route::apiResource('mesas', MesaController::class);
+Route::apiResource('reservas', ReservaController::class);
